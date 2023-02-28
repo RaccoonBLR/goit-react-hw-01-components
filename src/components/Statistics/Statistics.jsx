273301e6a00837e51cf9330ddaf578fa967b0809
Oleksandr <107481048+RaccoonBLR@users.noreply.github.com>
2023-02-28 15:09:35 +0200

    update file

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,35 @@
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
+import { getRandomHexColor } from 'js/getRandomHexColor';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.statlist}>
+        {stats.map(data => (
+          <li
+            key={data.id}
+            className={css.item}
+            style={{ backgroundColor: `${getRandomHexColor()}` }}
+          >
+            <span className={css.label}>{data.label}</span>
+            <span className={css.percentage}>{data.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,37 @@
+import css from './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
+
+export const TransactionHistory = ({ transactions }) => {
+  return (
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map(transaction => (
+          <tr key={transaction.id}>
+            <td>{transaction.type}</td>
+            <td>{transaction.amount}</td>
+            <td>{transaction.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  transaction: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
